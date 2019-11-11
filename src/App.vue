95486e8ff20ecbed4f9ba6f3025c85416ffe28e7
Alexander <izvekov.web@gmail.com>
2019-11-11 14:46:02 +0300

    Работа с vue router

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+
+    <!-- header -->
+
+    <div class="wrapper-content">
+      <section>
+        <div class="container"> 
+          <h1>Hello</h1>
+          <img alt="Vue logo" src="./assets/logo.png"> 
+        </div>
+      </section>
+    </div>
+
+     <!-- footer -->
+    
+  </div>
+</template>
+
+<script> 
+export default {} 
+</script> 
